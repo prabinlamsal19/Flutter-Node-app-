5b23e638f3d8161f6cfbc05b75b1ec924610639f
prabinlamsal19 <lamsalprabin74@gmail.com>
2022-11-04 23:06:56 +0545

    Project started.key concepts are noted in the notecopy

---
+++ server.js
@@ -0,0 +1,4 @@
+const express = require('express');     //importing express using require | 'express' object lai instantiate gareko express constant ma
+const app = express();                  //express() returns app
+
+app.listen(5000);                       //runs on port 5000
